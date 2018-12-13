44567894fe04b3a1d47abde59e49757cc4680f16
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2018-12-13 11:58:48 +0100

    Revert "Get rid of files I've found no use for, after all"
    This reverts commit 2cfff47cc739288b842b8ca864600f60032fed47.
    It turns out the Minimorph.Number module is used in package antfarm.

---
+++ NLP/Minimorph/Number.hs
@@ -0,0 +1,17 @@
+module NLP.Minimorph.Number where
+
+import Data.Text ( Text )
+
+-- | Singular and Plural.
+data SingPlu a = SP
+    { sg :: a
+    , pl :: a
+    }
+  deriving (Show, Eq)
+
+data Number = Singular | Plural
+  deriving (Eq, Show)
+
+fromSP :: Number -> SingPlu a -> a
+fromSP Singular = sg
+fromSP Plural   = pl

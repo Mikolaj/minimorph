2dbb6ab4f14fc68fb2d894905c1643698e1b589a
Mikolaj Konarski <mikolaj.konarski@funktory.com>
2020-06-12 23:06:44 +0200

    Refine the word gluing operation

--- NLP/Minimorph/Util.hs
+++ NLP/Minimorph/Util.hs
@@ -8,15 +8,17 @@ module NLP.Minimorph.Util
 import Data.Semigroup
 #endif
 
+import qualified Data.Char as Char
 import           Data.Text (Text)
 import qualified Data.Text as T
 
 infixr 6 <+>  -- matches Monoid.<>
 -- | Separated by space unless one of them is empty (in which case just
---   the non-empty one).
+--   the non-empty one) or the first ends or the last begins with whitespace.
 (<+>) :: Text -> Text -> Text
 t1 <+> t2 | T.null t1 = t2
           | T.null t2 = t1
+          | Char.isSpace (T.last t1) || Char.isSpace (T.head t2) = t1 <> t2
           | otherwise = t1 <> " " <> t2
 
 -- | Show a value in `Text` format.
